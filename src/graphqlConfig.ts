ba8979b5e05aadcc56bef5f532b523afced1bd3c
Gabriel Nordeborn <gabbe.nord@gmail.com>
2020-04-23 21:07:20 +0200

    Prepare for potential custom validation rules

--- src/graphqlConfig.ts
+++ src/graphqlConfig.ts
@@ -1,14 +1,28 @@
 import { GraphQLConfig, loadConfigSync } from "graphql-config";
 import { RelayDirectivesExtension } from "./configUtils";
+import * as path from "path";
 
 export function createGraphQLConfig(
-  workspaceBaseDir: string
+  workspaceBaseDir: string,
+  includeValidationRules?: boolean
 ): GraphQLConfig | undefined {
   const config = loadConfigSync({
     configName: "relay",
-    extensions: [RelayDirectivesExtension],
+    extensions: [
+      RelayDirectivesExtension,
+      () => ({
+        name: "customValidationRules",
+      }),
+    ],
     rootDir: workspaceBaseDir,
   });
+
+  if (includeValidationRules) {
+    const project = config.getProject();
+    project.extensions["customValidationRules"] = path.resolve(
+      path.join(__dirname, "../build/validationRules.js")
+    );
+  }
 
   return config;
 }
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { createGraphQLConfig } from "./graphqlConfig";
   try {
     await startServer({
       method: "node",
-      config: createGraphQLConfig(process.env.ROOT_DIR || ""),
+      config: createGraphQLConfig(process.env.ROOT_DIR || "", true),
       parser: (doc: string) => {
         const sources = extractGraphQLSources("reason", doc);
 
